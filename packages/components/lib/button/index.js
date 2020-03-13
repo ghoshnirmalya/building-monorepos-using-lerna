8f76650c0c049abb16898cac7d4f51d102ad78ee
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-03-13 23:12:27 +0530

    feat(app): Adding styles using Emotion

--- packages/components/lib/button/index.js
+++ packages/components/lib/button/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const Button = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button = styled.button`
+  padding: 12px 24px;
+  background-color: #121a3e;
+  font-size: 16px;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const _Button = ({ onClick, children }) => {
+  return <Button onClick={onClick}>{children}</Button>;
 };
 
-export default Button;
+export default _Button;
